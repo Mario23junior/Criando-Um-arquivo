bce2606b9a0603494412d0a1e5b2f2731c8c1e25
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-05 01:27:20 -0300

    Adicionado path de criação do arquivo

--- src/criandoArquivo/CreateData.java
+++ src/criandoArquivo/CreateData.java
@@ -1,7 +1,15 @@
 package criandoArquivo;
 
+import java.io.File;
+
 public class CreateData {
     
 	public static void main(String[] args) {
+		
+		try {
+			File file = new File("home/alienmj/arquivos/Arqivnew");
+			
+			
+		}
  	}
 }
