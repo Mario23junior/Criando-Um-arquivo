4cb3d4ce2e8e69bf3d295d6e4b99ac4dbb428718
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-05 01:32:56 -0300

    Adicionando logica interna de criação de arquivos

--- src/criandoArquivo/CreateData.java
+++ src/criandoArquivo/CreateData.java
@@ -1,15 +1,22 @@
 package criandoArquivo;
 
 import java.io.File;
+import java.io.IOException;
 
 public class CreateData {
     
 	public static void main(String[] args) {
 		
 		try {
-			File file = new File("home/alienmj/arquivos/Arqivnew");
-			
-			
-		}
+			File file = new File("/home/alienmj/arquivos/Arqivnew.txt");
+			boolean dataArquivo = file.createNewFile();
+			if(dataArquivo) {
+				System.out.println("Arquivo criado com sucesso");
+			}else {
+				System.out.println("Falha ao criar arquivo");
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+ 		}
  	}
 }
